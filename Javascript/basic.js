e4fec9b9fc12f71f79940f5687b6c098e7cc28d6
Amanpreet Singh <114504509+AmanSingh-77@users.noreply.github.com>
2025-01-29 21:57:03 +0530

    Javascript basics

---
+++ Javascript/basic.js
@@ -0,0 +1,28 @@
+// Print statement
+console.log("Hello World")
+
+// Declaring variables
+var a = "Red"
+var b = 1;
+var c = 2;
+
+console.log("The color of door is ",a);
+
+// Artithematic operator (+)
+console.log("Value of 1 + 2 is ",b+c);
+
+// length and typeof() methods
+console.log("Length of variable a is ",a.length);
+console.log("Variable a is of type",typeof(a));
+
+
+//Concatenation
+var d = "Aman"
+var e = "preet"
+var f = "Singh"
+
+console.log("My name is ", d+e+f);
+
+
+
+
---
+++ Javascript/odd-even.js
@@ -0,0 +1,31 @@
+function odd_even(num){
+    if(num%2==0){
+        console.log("Even")
+    }
+
+    else{
+        console.log("Odd");
+        
+    }
+}
+
+odd_even(10);
+odd_even(11);
+odd_even(13);
+
+console.log("Arrow Function");
+
+let check=(num2)=>{
+    if(num2%2==0){
+        console.log("Even")
+    }
+
+    else{
+        console.log("Odd");
+        
+    }
+}
+
+odd_even(21);
+odd_even(28);
+odd_even(9);
