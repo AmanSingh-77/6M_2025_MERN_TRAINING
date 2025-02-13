b8b7b457c5edf43cc50a9913fde6f3c67a6bf826
Amanpreet Singh <114504509+AmanSingh-77@users.noreply.github.com>
2025-02-13 22:51:30 +0530

    Intro using react

---
+++ React Projects/introduction/src/components/Intro.jsx
@@ -0,0 +1,12 @@
+export default function Intro(){
+    return(
+        <div>
+            <h1 style={{fontSize:'6vh'}}>Amanpreet Singh</h1>
+            <p style={{fontSize:'3vh'}}>This is my introduction page using react and the project is created using vite.</p>
+            <p style={{fontSize:'3vh'}}>Also, I think I am a full stack developer ('_')</p>
+            <br/>
+            <hr/>
+            <br/>
+        </div>
+    )
+}
