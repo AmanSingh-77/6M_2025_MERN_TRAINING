0e9af4f353d5143d19f54cb44c6fdeefaf29547f
Amanpreet Singh <114504509+AmanSingh-77@users.noreply.github.com>
2025-02-13 22:53:40 +0530

    react basics

---
+++ React Projects/basics/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Hi, I am Amanpreet Singh and this is my introduction page using React.</p>
+      <p>I am a student of Guru Nanak Dev University, Regional Campus, Jalandhar. I am currently pursuing B.Tech in Computer Science and Engineering.</p>
+      <p>I am currently training for MERN Stack development from o7 Services.</p>
+      <p>I like to play basketball and listen to my favourite music.</p>
+    </div>
+  );
+}
+
+export default App;
