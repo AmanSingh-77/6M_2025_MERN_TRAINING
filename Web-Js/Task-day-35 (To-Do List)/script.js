690ef16b6618937be1ad5a95801624ff60a3409e
Amanpreet Singh <114504509+AmanSingh-77@users.noreply.github.com>
2025-02-13 20:53:56 +0530

    To-do list using js

---
+++ Web-Js/Task-day-35 (To-Do List)/script.js
@@ -0,0 +1,59 @@
+// count variable to set id's
+count = 0
+
+// Add button
+function add(){
+    var a = document.getElementById("input").value
+        
+    divs = document.createElement("div")   
+    document.getElementById('box').appendChild(divs)
+    divs.id = count
+    document.getElementById(`${divs.id}`).classList.add('row')
+
+    //for all the lists except first one
+    if(divs.id!=0){
+        document.getElementById(`${divs.id}`).innerHTML = `
+        <div class="col-7 p-0">
+                    <span id ="${divs.id}Span" class=" ps-4 ms-3 fw-bold">${a}</span>
+                </div>
+
+                <div class="col p-0 ">
+                    <button onclick="edit('${divs.id}')" class="button me-2">Edit</button>
+                    <button onclick="deleted('${divs.id}')" class="button text-danger">Delete</button>
+                </div>`
+                document.getElementById(`${(divs.id - 1)}`).innerHTML += `<hr class="my-3 w-75 mx-auto"></hr>`
+    }
+
+    //for first list
+    else{
+        document.getElementById(`${divs.id}`).innerHTML = `<div class="col-7 p-0">
+                    <span id ="${divs.id}Span" class=" ps-4 ms-3 fw-bold">${a}</span>
+                </div>
+
+                <div class="col p-0 ">
+                    <button onclick="edit('${divs.id}')" class="button me-2">Edit</button>
+                    <button onclick="deleted('${divs.id}')" class="button text-danger">Delete</button>
+                </div>
+                `
+    }
+    
+    count++
+
+    document.getElementById("input").value = ""    
+}
+
+// Edit Button
+function edit(id){
+    spanTxt = document.getElementById(`${id}Span`).innerHTML
+    newPrompt = prompt('Edit Your To-Do list',`${spanTxt}`)
+    checkPrompt = newPrompt.trim()
+    if(checkPrompt!==''){
+        document.getElementById(`${id}Span`).innerHTML = newPrompt
+    }
+}
+
+// Delete Button
+function deleted(id){
+  a = document.getElementById(id)
+  a.remove()
+}
