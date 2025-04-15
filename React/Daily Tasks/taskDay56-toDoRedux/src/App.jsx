5177d0511f9bb65cbe18be1ad67c4bccd92fc0bd
Amanpreet Singh <114504509+AmanSingh-77@users.noreply.github.com>
2025-04-16 00:05:24 +0530

    redux to do list

--- React/Daily Tasks/taskDay56-toDoRedux/src/App.jsx
+++ React/Daily Tasks/taskDay56-toDoRedux/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import AddList from './Components/AddList';
+import ShowLists from './Components/ShowLists';
+import Master from './Components/layouts/Master';
+import Home from './Components/Home';
+import { ToastContainer} from 'react-toastify';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Master/>}>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/add' element={<AddList/>}/>
+            <Route path='/show' element={<ShowLists/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+        <ToastContainer position="top-right" autoClose={1500} />
     </>
   )
 }
